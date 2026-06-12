ce38915e31e45738bb86a23952134968affbfaee
Shavali Badvel <55553298+shavalibadvel@users.noreply.github.com>
2026-06-12 13:37:44 +0000

    changes

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -8,7 +8,7 @@ const getCurrentDate = () => {
   const day = String(currentDate.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 };
-
+//gityrgiuhregiuyeb
 const TodoInput = (/*{ isDarkMode }*/) => {
   const [todoTitle, setTodoTitle] = useState("");
   const [dueDate, setDueDate] = useState(getCurrentDate());
